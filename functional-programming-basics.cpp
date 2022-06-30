703a0cfb512161ea5151e96dff6b964cd85d91ef
Frank Ronnfeldt <44884020+ronnfran@users.noreply.github.com>
2022-07-01 00:26:57 +1000

    Create functional-programming-basics.cpp
    should cover the basics of functional programming in the C language. Functions covered should include, at the least, map(), filter(), reduce() and information on immutable data structures in the language.

---
+++ functional-programming-basics.cpp
@@ -0,0 +1,5 @@
+/*
+should cover the basics of functional programming in the C++ language. Functions covered should include, at the least, map(), filter(), reduce() and information on immutable data structures in the language.
+
+
+*/
