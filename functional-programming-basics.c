ea6e203c2eacb68f108440a76a9000cbecae1579
Frank Ronnfeldt <44884020+ronnfran@users.noreply.github.com>
2022-07-01 00:26:07 +1000

    Create functional-programming-basics.c
    should cover the basics of functional programming in the C language. Functions covered should include, at the least, map(), filter(), reduce() and information on immutable data structures in the language.

---
+++ functional-programming-basics.c
@@ -0,0 +1,3 @@
+/*
+should cover the basics of functional programming in the C language. Should include, at the least, information on useful ubiquitous functions in the functional programming paradigm and information on immutable data structures in the language.
+*/
